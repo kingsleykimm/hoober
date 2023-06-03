68d8b7f83416a774d87e3c267571204ff089b16e
Kingsley Kim <kingsleykimm@gmail.com>
2023-06-03 00:40:02 -0400

    Switched to MERN stack but replaced M -> SQL, final switch

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const PORT = process.env.PORT
+// middleware
+const corsOptions = {
+    origin: "http://localhost:3000" // frontend URI (ReactJS)
+}
+app.use(express.json());
+app.use(cors(corsOptions));
+app.get("/", (req, res) => {
+    res.status(201).json({message: "Connected to Backend!"});
+});
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
