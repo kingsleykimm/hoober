ab38e963d92367745fab27fae5d713444af6e968
Kingsley Kim <kingsleykimm@gmail.com>
2023-06-05 04:15:16 -0400

    Finished Sign Up Form module with styling and validation

--- frontend/src/components/Signupform.js
+++ frontend/src/components/Signupform.js
@@ -1,14 +1,25 @@
-import React from "react"
+import React, { useRef } from "react"
+import { useForm } from "react-hook-form"
+import {yupResolver} from "@hookform/resolvers/yup"
+import * as Yup from 'yup'
+function Signupform() {
+    const formSchema = Yup.object().shape({
+        password: Yup.string()
+          .required('Password is required')
+          .min(6, 'Password must be at 6 characters long'),
+        confirmPassword: Yup.string()
+          .required('Password is required')
+          .oneOf([Yup.ref('password')], 'Passwords do not match'),
+      })
+    const formOptions = {resolver: yupResolver(formSchema)}
+    const { register, handleSubmit, formState } = useForm(formOptions);
+    const {errors} = formState
 
-import {useForm} from "react-hook-form"
-function Signupform () {
-    const {register, handleSubmit, watch} = useForm();
+
 
     const onSubmit = async (data) => {
         const requestOptions = {
             method: "POST",
-
-
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data)
         }
@@ -18,24 +29,52 @@ function Signupform () {
             console.log(responseData)
             return responseData
         }
-        catch (error) { console.log ("Error: " + error)}
+        catch (error) { console.log("Error: " + error) }
     }
 
     return (
+        <div className="form--wrapper">
 
-        <form
-        // action="/localhost:4000"
-        onSubmit= {handleSubmit(onSubmit)}
-        >
-            <input 
-            placeholder="Username" 
-            {...register("username", {required : true})}
-            />
-            <input placeholder="Password" {...register("password", {required: true})}></input>
-            <input placeholder="Confirm Password" {...register("confirmPassword", {required : true})}></input>
-            <input value="Sign Up" type="submit"></input>
-
-        </form>
+            <form className="user--form"
+                // action="/localhost:4000"
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <h1
+                    style={
+                        {
+                            "fontFamily": "Inter",
+                            "color": "#F4EEE0",
+                            "alignSelf": "center"
+                        }
+                    }
+                >Sign Up</h1>
+                <label className="form--label">Username</label>
+                <input
+                    placeholder="Username"
+                    name="username"
+                    {...register("username")}                    
+                />
+                <label className="form--label">Password</label>
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    {...register("password")}
+                   ></input>
+                <div className="form--label"> {errors.password?.message}</div>
+                <label className="form--label">Confirm Password</label>
+                <input style={{ 'marginBottom': "20px" }}
+                    placeholder="Confirm Password"
+                    name="confirmPassword"
+                    type="password"
+                    {...register("confirmPassword")}
+                ></input>
+                <div className="form--label">{errors.confirmPassword?.message}</div>
+                <input value="Sign Up" type="submit"
+                ></input>
+                
+            </form>
+        </div>
     )
 
 }
